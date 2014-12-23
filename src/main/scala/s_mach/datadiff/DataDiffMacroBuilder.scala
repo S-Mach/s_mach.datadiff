0baa2d423458fd990eb89d5d730597be2e9e4c8f
Lance Gatlin <lance.gatlin@gmail.com>
2014-12-23 11:27:48 -0500

    update readme [skip ci]

--- src/main/scala/s_mach/datadiff/DataDiffMacroBuilder.scala
+++ src/main/scala/s_mach/datadiff/DataDiffMacroBuilder.scala
@@ -10,7 +10,7 @@
          1t1i   .;;   .1tf       ___/ //___// /  / // /_/ // /__ / / / /
        CLt1i    :,:    .1tfL.   /____/     /_/  /_/ \__,_/ \___//_/ /_/
        Lft1,:;:       , 1tfL:
-       ;it1i ,,,:::;;;::1tti      s_mach.codetools
+       ;it1i ,,,:::;;;::1tti      s_mach.datadiff
          .t1i .,::;;; ;1tt        Copyright (c) 2014 S-Mach, Inc.
          Lft11ii;::;ii1tfL:       Author: lance.gatlin@gmail.com
           .L1 1tt1ttt,,Li
