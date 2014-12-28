16670ba49e4cd1dd171081d10fae5a31b042cf7e
Lance Gatlin <lance.gatlin@gmail.com>
2014-12-28 00:15:18 -0500

    added DataDiff for Char and String

--- src/main/scala/s_mach/datadiff/AllBuiltInDataDiffImplicits.scala
+++ src/main/scala/s_mach/datadiff/AllBuiltInDataDiffImplicits.scala
@@ -21,13 +21,15 @@ package s_mach.datadiff
 import s_mach.datadiff.impl.SimpleDataDiff
 
 trait AllBuiltInDataDiffImplicits {
+  implicit object DataDiff_Boolean extends SimpleDataDiff[Boolean]
   implicit object DataDiff_Byte extends SimpleDataDiff[Byte]
   implicit object DataDiff_Short extends SimpleDataDiff[Short]
   implicit object DataDiff_Int extends SimpleDataDiff[Int]
   implicit object DataDiff_Long extends SimpleDataDiff[Long]
   implicit object DataDiff_Float extends SimpleDataDiff[Float]
   implicit object DataDiff_Double extends SimpleDataDiff[Double]
+  implicit object DataDiff_Char extends SimpleDataDiff[Char]
+  implicit object DataDiff_String extends SimpleDataDiff[String]
   implicit object DataDiff_BigInt extends SimpleDataDiff[BigInt]
   implicit object DataDiff_BigDecimal extends SimpleDataDiff[BigDecimal]
-  implicit object DataDiff_String extends SimpleDataDiff[String]
 }
